9a6ccff333238f58cf3ce32b82aa8ad5937aa7b7
umemiya969 <elanie477@gmail.com>
2025-12-16 13:11:55 +0800

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -6,7 +6,7 @@ pub enum MessageType {
     JobAnnouncement,
     JobResultSubmission,
     JobVerificationVote,
-    BlockBroadcast,
+    BlockBroadcast, // belum dipakai
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -21,4 +21,15 @@ pub struct NetworkEnvelope {
 pub struct Job {
     pub job_id: Uuid,
     pub payload: String,
+    pub required_votes: usize,
+    pub finalized: bool,
+    pub final_result: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Vote {
+    pub job_id: Uuid,
+    pub worker_pubkey: String,
+    pub result_hash: String,
+    pub signature: String,
 }
